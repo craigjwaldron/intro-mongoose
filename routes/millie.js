d2fdd414bc097592343d442c840a2f235eafec70
Craig <craigjwaldron@gmail.com>
2016-06-30 11:19:22 -0500

    one route rerouted

---
+++ routes/millie.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var router = express.Router();
+
+var path = require ('path');
+var User = require('../models/user');
+
+//dummy-value get route. Dummy value meaning
+//'hard coded' Millie
+router.get('/', function(req, res) {
+  console.log('in millie get');
+  var millie = new User({
+    name: 'Millie',
+    username: 'millie11',
+    password: 'reallybadpassword'
+  });
+
+  millie.save(function(err) {
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }else{
+      console.log('User saved successfully!');
+      res.sendStatus(200);
+    }
+  });
+});//end millie get route
+
+//update millie route
+router.put('/updateMillie', function(req, res) {
+  console.log('hit put route - updateMillie');
+
+  //Hard coded to find one user with name Millie
+  User.findOne({name: 'Millie'}, function(err, userResult) {
+    console.log('find user result = ', userResult);
+
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }else{
+      userResult.name = "Millie Walsh";
+      userResult.admin = false;
+
+      userResult.save(function(err) {
+        if(err){
+          console.log(err);
+          res.sendStatus(500);
+        }else{
+          console.log('Update user = ', userResult._id);
+          res.sendStatus(200);
+          /**
+            Thought: What do you want your API to return.
+            Just a status code? The updated user object?
+            Or the userResult._id? res.send(userResult);
+          **/
+        }
+      });
+    }
+  });
+}); //end update Millie route
+
+/**
+  Thought: How could you make this route delete any user?
+  It will need to take something to uniquely identify the user.
+  This should be passed in the URL. For ex: localhost:3000/delete/<id>
+**/
+// delete Millie route
+router.delete('/deleteMillie', function(req, res) {
+  console.log('delete route');
+
+  User.findOne({username: 'millie11'}, function(err, userResult) {
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }else{
+      User.remove({_id: userResult._id}, function(err) {});
+      res.sendStatus(200);
+    }
+  });
+});// end delete route
+
+module.exports = router;
--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
+var path = require('path');
 //require model
-var User = require('../models/user');
 
 // get the express app
 var app = express();
+
+//require routes
+var millie = require ('../routes/millie');
+var all = require ('../routes/all');
+
+//use routes
+app.use('/millie', millie);
 
 //connect to the database - userDb is the database name
 mongoose.connect('mongodb://localhost:27017/userDb');
@@ -14,25 +20,7 @@ mongoose.connect('mongodb://localhost:27017/userDb');
 //parse json
 app.use(bodyParser.json());
 
-//dummy-value get route. Dummy value meaning
-//'hard coded' Millie
-app.get('/millie', function(req, res) {
-  var millie = new User({
-    name: 'Millie',
-    username: 'millie11',
-    password: 'reallybadpassword'
-  });
-
-  millie.save(function(err) {
-    if(err){
-      console.log(err);
-      res.sendStatus(500);
-    }else{
-      console.log('User saved successfully!');
-      res.sendStatus(200);
-    }
-  });
-});//end millie get route
+//enable router
 
 //get all users
 app.get('/all', function(req, res) {
@@ -68,63 +56,7 @@ app.post('/create', function(req, res) {
   });
 });
 
-/**
-  Thought: How could you make this route update any user?
-  It will need to take something to uniquely id the user as
-  well as the updated user object.
-**/
-//update millie route
-app.put('/updateMillie', function(req, res) {
-  console.log('hit put route - updateMillie');
 
-  //Hard coded to find one user with name Millie
-  User.findOne({name: 'Millie'}, function(err, userResult) {
-    console.log('find user result = ', userResult);
-
-    if(err){
-      console.log(err);
-      res.sendStatus(500);
-    }else{
-      userResult.name = "Millie Walsh";
-      userResult.admin = false;
-
-      userResult.save(function(err) {
-        if(err){
-          console.log(err);
-          res.sendStatus(500);
-        }else{
-          console.log('Update user = ', userResult._id);
-          res.sendStatus(200);
-          /**
-            Thought: What do you want your API to return.
-            Just a status code? The updated user object?
-            Or the userResult._id? res.send(userResult);
-          **/
-        }
-      });
-    }
-  });
-}); //end update Millie route
-
-/**
-  Thought: How could you make this route delete any user?
-  It will need to take something to uniquely identify the user.
-  This should be passed in the URL. For ex: localhost:3000/delete/<id>
-**/
-// delete Millie route
-app.delete('/deleteMillie', function(req, res) {
-  console.log('delete route');
-
-  User.findOne({username: 'millie11'}, function(err, userResult) {
-    if(err){
-      console.log(err);
-      res.sendStatus(500);
-    }else{
-      User.remove({_id: userResult._id}, function(err) {});
-      res.sendStatus(200);
-    }
-  });
-});// end delete route
 
 // server listen
 var server = app.listen(3000, function() {
